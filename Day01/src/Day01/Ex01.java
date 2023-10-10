cb5b6efbc77187bbe09c07faa5e0645cb5443a38
JiWoongE <rlawldnd515@kyonggi.ac.kr>
2023-10-10 23:58:37 +0900

    Update Ex01.java

--- Day01/src/Day01/Ex01.java
+++ Day01/src/Day01/Ex01.java
@@ -1,10 +1,24 @@
 package Day01;
 
+class TV {
+	String name;
+	int year;
+	int size;
+	public TV(String name,int year,int size) {
+		this.name=name;
+		this.year=year;
+		this.size=size;
+	}
+	public void show() {
+		System.out.print(name+"에서 만든 "+year+"년형 "+size+"인치 TV");
+	}
+}
+
 public class Ex01 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World !!");
-
+		TV myTV = new TV("LG",2017,32);
+		myTV.show();
 	}
 
 }
