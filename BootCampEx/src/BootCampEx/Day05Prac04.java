a79548a885d97387431e8a2bd88dbecfd2a12d81
JiWoongE <rlawldnd515@kyonggi.ac.kr>
2024-01-08 10:19:58 +0900

    Create Day05Prac04.java

---
+++ BootCampEx/src/BootCampEx/Day05Prac04.java
@@ -0,0 +1,57 @@
+package BootCampEx;
+
+class Point1 {
+    private int x, y;
+
+    public Point1(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    protected void move(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class PositivePoint extends Point1 {
+
+    public PositivePoint() {
+        super(0, 0); 
+    }
+
+    public PositivePoint(int x, int y) {
+        super(x > 0 ? x : 0, y > 0 ? y : 0);
+    }
+
+    @Override
+    protected void move(int x, int y) {
+       if (x>0 && y>0) {super.move(x,y);}
+    }
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "("+getX()+","+getY()+")의 점";
+	}
+}
+
+public class Day05Prac04 {
+
+    public static void main(String[] args) {
+    	   PositivePoint p = new PositivePoint();
+    	   p.move(10, 10);
+    	   System.out.println(p.toString()+"입니다.");
+    	   p.move(-5,5); 
+    	   System.out.println(p.toString()+"입니다.");
+    	   PositivePoint p2 = new PositivePoint(-10, -10);
+    	   System.out.println(p2.toString()+"입니다.");
+
+    }
+}
